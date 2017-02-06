1f48e0af4dcf4e462cf0f1a615d64f09f95c8883
abe <abestbet1@gmail.com>
2017-02-06 14:31:30 -0800

    fix error registering app

--- index.ios.js
+++ index.ios.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 import App from './src/App'
 
 
-AppRegistry.registerComponent('manager', () => App);
+AppRegistry.registerComponent('ticket_native', () => App);
